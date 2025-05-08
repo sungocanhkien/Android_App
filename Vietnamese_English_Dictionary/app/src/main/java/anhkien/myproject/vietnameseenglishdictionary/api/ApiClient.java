3394bf98d6c304448993ed73f0542689c865bef4
sungocanhkien <kiensu95@gmail.com>
2025-05-08 22:33:07 +0700

    update

---
+++ Vietnamese_English_Dictionary/app/src/main/java/anhkien/myproject/vietnameseenglishdictionary/api/ApiClient.java
@@ -0,0 +1,17 @@
+package anhkien.myproject.vietnameseenglishdictionary.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    
+    public static Retrofit getRetrofit() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("https://api.dictionaryapi.dev/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
