c1e7222f34a615f8ea188544d20f9c6deefdcf29
sungocanhkien <kiensu95@gmail.com>
2025-05-21 01:58:19 +0700

    update

--- Vietnamese_English_Dictionary/app/src/main/java/anhkien/myproject/vietnameseenglishdictionary/SplashActivity.java
+++ Vietnamese_English_Dictionary/app/src/main/java/anhkien/myproject/vietnameseenglishdictionary/SplashActivity.java
@@ -32,6 +32,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 1000 );
+        }, 3000 );
     }
 }
